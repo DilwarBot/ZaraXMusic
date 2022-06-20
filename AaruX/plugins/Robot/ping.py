98b212b5c51710a39e5d91ea2972c066eece0e31
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-20 20:09:37 +0530

    Update ping.py

--- AaruX/plugins/Robot/ping.py
+++ AaruX/plugins/Robot/ping.py
@@ -5,10 +5,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-from AnonX import app
-from AnonX.core.call import Anon
-from AnonX.utils import bot_sys_stats
-from AnonX.utils.decorators.language import language
+from AaruX import app
+from AaruX.core.call import Anon
+from AaruX.utils import bot_sys_stats
+from AaruX.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
