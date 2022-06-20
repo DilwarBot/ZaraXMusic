079caa249cde4153fe4cee088d3de5d573df71fd
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-20 20:08:37 +0530

    Update play.py

--- AaruX/plugins/Robot/play.py
+++ AaruX/plugins/Robot/play.py
@@ -10,21 +10,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from AaruX import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from AnonX.core.call import Anon
-from AnonX.utils import seconds_to_min, time_to_seconds
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.database import is_video_allowed
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.decorators.play import PlayWrapper
-from AnonX.utils.formatters import formats
-from AnonX.utils.inline.play import (livestream_markup,
+from AaruX.core.call import Anon
+from AaruX.utils import seconds_to_min, time_to_seconds
+from AaruX.utils.channelplay import get_channeplayCB
+from AaruX.utils.database import is_video_allowed
+from AaruX.utils.decorators.language import languageCB
+from AaruX.utils.decorators.play import PlayWrapper
+from AaruX.utils.formatters import formats
+from AaruX.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from AnonX.utils.inline.playlist import botplaylist_markup
-from AnonX.utils.logger import play_logs
-from AnonX.utils.stream.stream import stream
+from AaruX.utils.inline.playlist import botplaylist_markup
+from AaruX.utils.logger import play_logs
+from AaruX.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
