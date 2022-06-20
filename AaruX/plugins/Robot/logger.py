ba17b7f5ae72fe9b227dc0cf9e234dc31c2b9436
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-20 20:14:15 +0530

    Update logger.py

--- AaruX/plugins/Robot/logger.py
+++ AaruX/plugins/Robot/logger.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import add_off, add_on
-from AnonX.utils.decorators.language import language
+from AaruX import app
+from AaruX.misc import SUDOERS
+from AaruX.utils.database import add_off, add_on
+from AaruX.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
