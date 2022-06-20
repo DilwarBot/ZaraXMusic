8969be7dca047966a2e090d0cb7747495a2fbaa1
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-20 18:38:10 +0530

    Update channelplay.py

--- AaruX/utils/channelplay.py
+++ AaruX/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonX import app
-from AnonX.utils.database import get_cmode
+from AaruX import app
+from AaruX.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
