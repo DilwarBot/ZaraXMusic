d51b4ae71f06d48208cadf63a5da50dd7d0ee4cd
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 20:13:34 +0530

    Update __main__.py

--- AaruX/__main__.py
+++ AaruX/__main__.py
@@ -53,7 +53,7 @@ async def init():
     await Anon.start()
     try:
         await Anon.stream_call(
-            "https://telegra.ph/file/8d5db123638c2f6bb6ce4.mp4"
+            "https://telegra.ph/file/cdb9e510b571ad5758bd9.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AaruX").error(
